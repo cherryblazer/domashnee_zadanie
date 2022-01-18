41befcf8e89138fdf9889d9ecae70b16ac810ab0
cherryblazer <70815027+cherryblazer@users.noreply.github.com>
2022-01-19 00:23:32 +0600

    Delete dz_7_1.py

--- dz_7_1.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def main():
-    os.system('mkdir my_project')
-    os.system('mkdir my_project/settings')
-    os.system('mkdir my_project/mainapp')
-    os.system('mkdir my_project/adminapp')
-    os.system('mkdir my_project/authapp')
